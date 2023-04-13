a97e32b71e7c8eed977adbaf23edd71a74271647
singhJasmeet159 <singhjasmeet2506@gmail.com>
2023-04-13 18:56:11 +1000

    functions for rest api calls added

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -9,4 +9,44 @@ const apiService = axios.create({
   },
 });
 
+export const getCompanies = async () => {
+  return await apiService.get("/companies");
+};
+
+export const getEmployees = async () => {
+  return await apiService.get("/employees");
+};
+
+export const getTrucks = async () => {
+  return await apiService.get("/trucks");
+};
+
+export const getTrailers = async () => {
+  return await apiService.get("/trailers");
+};
+
+export const getJobs = async () => {
+  return await apiService.get("/jobs");
+};
+
+export const getJobById = async (jobId: string) => {
+  return await apiService.get(`/jobs/${jobId}`);
+};
+
+export const createJob = async (jobData: object) => {
+  return await apiService.post("/jobs", jobData);
+};
+
+export const updateJob = async (jobId: string, jobData: object) => {
+  return await apiService.put(`/jobs/${jobId}`, jobData);
+};
+
+export const deleteJob = async (jobId: string) => {
+  return await apiService.delete(`/jobs/${jobId}`);
+};
+
+export const sendJobEmail = async (jobId: string) => {
+  return await apiService.post(`/jobs/${jobId}/email`);
+};
+
 export default apiService;
